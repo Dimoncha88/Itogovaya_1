eb39fe2b9ee1efc3e4075039a8440705a26976c2
Dmitry <dimonch_rus@mail.ru>
2022-12-29 14:29:26 +0300

    add Example 02

---
+++ Example02/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+string[] arrayString = { "1234", "1567", "-2", "computer science" };
+
+Console.Write($"Введенный массив: [ {string.Join(" ", arrayString)} ]");
+Console.WriteLine();
+
+string[] newString = FindElementsLessThenValue(arrayString);
+
+string[] FindElementsLessThenValue(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    int value = 3;
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= value)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+        else
+            j = j * 1;
+    }
+    return newArray;
+}
+
+string[] PrintArray(string[] inputArray)
+{
+    int count = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i] != null) count++;
+    }
+
+    int j = 0;
+    string[] resultArray = new string[count];
+    for (int i = 0; i < count; i++)
+    {
+        resultArray[j] = inputArray[i];
+        j++;
+    }
+    return resultArray;
+}
+Console.WriteLine();
+Console.Write($"Получившийся массив: [ {String.Join(" ", PrintArray(newString))} ]");
